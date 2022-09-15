1c1302a5e7f1d9c7fe4766b30c82164b4ec32db9
karreycodes <kamondewangari@gmail.com>
2022-09-15 10:13:56 -0700

    minor changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,15 @@
  * more_numbers-prints numbers to 14
  * Return-returns 0
  */
-void more_numbers(void)
+void more_numbers (void)
 {
 	int n1, n2;
 
-	for(n1 = 0; n1 < 10; n1++)
+	for (n1 = 0; n1 < 10; n1++)
 	{
-		for(n2 = 0; n2 <= 14; n2++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			if(n2 > 9)
+			if (n2 > 9)
 			{
 				putchar((n2 / 10) + '0');
 			}
