ea04d586a846692531426f9fc0e81c2c19d6d636
karreycodes <kamondewangari@gmail.com>
2022-11-02 05:48:00 -0700

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 on success, -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content);
 {
 	int r, k, e = 0;
 
